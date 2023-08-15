77dfd5b75b7c2beb97a1be4e17994d6e94c19286
pukiskun <fatalitycloud@gmail.com>
2023-08-15 11:04:51 +0700

    code(1)

--- resources/views/firebase/actions.blade.php
+++ resources/views/firebase/actions.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,11 +9,14 @@ initial-scale=1.0">
     <title>{{ $pageTitle }}</title>
     @vite('resources/sass/app.scss')
 </head>
+
 <body>
     <div class="d-flex">
         <a href="{{ url('edit/' . $key) }}" class="btn btn-warning px-xl-4">Edit</a>
+        <a href="{{ url('detail/' . $key) }}" class="btn btn-primary px-xl-4">QR Code</a>
         <a href="{{ url('delete/' . $key) }}" class="btn btn-danger px-sm-4">Delete</a>
     </div>
     @vite('resources/js/app.js')
 </body>
+
 </html>
